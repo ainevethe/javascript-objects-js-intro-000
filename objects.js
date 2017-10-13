c0454efdd4081ea63270068199dd5526757a2c50
Crista Cooper <aine3d@gmail.com>
2017-10-13 01:05:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {"Elvis": "Hound Dog", "Volbeat":"Warriors Call"};
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
-  return playlist
+  return playlist;
 }
